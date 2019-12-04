485befe90b9e11970f7107aac630fb8a94238100
Enes koç <32553624+eneskocc@users.noreply.github.com>
2019-12-04 17:08:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,5 @@
 ymaps.ready(init);
-
-
-
-
-
-
+let pano = "";
 function init() {
     var myMap = new ymaps.Map("map", {
         center: [39.43, 35.30],
@@ -12,22 +7,37 @@ function init() {
     }, {
         searchControlProvider: 'yandex#search'
     });
-
     var xmlhttp = new XMLHttpRequest();
     xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             var myObj = JSON.parse(this.responseText);
             myObj.result.map(deprem => {
+                pano += `
+                <div class="swiper-slide border border-danger mt-3 text-center p-2 rounded">
+                    <p class="text-danger">${deprem.title}</p>
+                    <p>${deprem.mag} büyüklüğnde</p>
+                    <p>${deprem.date}</p>
+                </div>
+                `;
                 myMap.geoObjects
                     .add(new ymaps.Placemark([deprem.lat, deprem.lng], {
-                        balloonContent: deprem.title + `<br>`+deprem.mag+" Büyüklüğünde "
+                        balloonContent: deprem.title + `<br>` + deprem.mag + " büyüklüğünde " + `<br>` + deprem.date + `<br>` + deprem.depth + " km derinliğinde"
+
                     }, {
                         preset: 'islands#circleDotIcon',
                         iconColor: 'red'
                     }))
             });
+            document.getElementById("pano").innerHTML = pano;
+         
         }
+
     };
+
     xmlhttp.open("GET", "https://api.orhanaydogdu.com.tr/deprem/live.php", true);
     xmlhttp.send();
+
 }
+
+
+
